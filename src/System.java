2b892c5fa7fd7b7ff449f7821f8d8a63e5499e44
Zhur.a.vleva <Zhur.a.vleva.Darya21@gmail.com>
2022-04-23 15:53:03 +0300

    [ADDED]: server methods, timetable methods

--- src/System.java
+++ src/System.java
@@ -2,6 +2,6 @@ public class System {
 
     // global variables
     public static final Server server = new Server();
-
+    public static final String ADMIN_LOGIN = "gefkmld&vedfn#";
     // use cases of our program
 }
--- src/TimeTable.java
+++ src/TimeTable.java
@@ -1,2 +1,6 @@
 public class TimeTable {
+
+    public void printSchedule(List<Departure>) {
+        //TODO(Print info)
+    }
 }
