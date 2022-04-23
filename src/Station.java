2830abc3bf2993b5c9c72eb6cc621f6a1e79f4fe
hermandyudin <dyudin.german@mail.ru>
2022-04-23 16:18:09 +0300

    [CHANGED] : class Station

--- src/Station.java
+++ src/Station.java
@@ -4,12 +4,28 @@ public class Station {
     private String name;
     private Admin admin;
     private ArrayList<TimeTable> timeTables;
-    
 
     public String getName(){
         return this.name;
     }
+
     public void setName(String newName){
         this.name = newName;
+    }
+
+    public Admin getAdmin(){
+        return this.admin;
+    }
+
+    public void setAdmin(Admin newAdmin){
+        this.admin = newAdmin;
+    }
+
+    public void addTimeTable(TimeTable t){
+        this.timeTables.add(t);
+    }
+
+    public void removeTimeTable(TimeTable t){
+        this.timeTables.remove(t);
     }
 }
