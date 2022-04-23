17557529c6516261bc56eb3a3f400687a77f6d91
hermandyudin <dyudin.german@mail.ru>
2022-04-23 16:05:41 +0300

    [ADDED] class Departure, [CHANGED] class Station

--- src/Station.java
+++ src/Station.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class Station {
     private String name;
+    private Admin admin;
+    private ArrayList<TimeTable> timeTables;
+    
 
     public String getName(){
         return this.name;
