02380520d2f2dba9e7256dfb63f9bb138035e3fc
hermandyudin <dyudin.german@mail.ru>
2022-04-23 16:34:37 +0300

    [CHANGED] class Departure : added field and constructor

--- src/Departure.java
+++ src/Departure.java
@@ -1,3 +1,15 @@
-public class Departure {
+import java.time.LocalDate;
 
+public class Departure {
+    private LocalDate departedTime;
+    private LocalDate expectedArrivalTime;
+    private Train train;
+    private Station toStation;
+
+    public Departure(LocalDate departedTime, LocalDate expectedArrivalTime, Train train, Station toStation){
+        this.departedTime = departedTime;
+        this.expectedArrivalTime = expectedArrivalTime;
+        this.train = train;
+        this.toStation = toStation;
+    }
 }
